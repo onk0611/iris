e6d3b8ce68161a6deb91944cbd02fc6a703712f7
ONK <theos.pro@proton.me>
2023-03-08 14:01:43 +0100

    add plugin template

---
+++ wp-content/plugins/TEST_IRIS_INTERACTIVE/admin/partials/technical test by théo soulier for iris interactive-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://theo-soulier.fr
+ * @since      1.0.0
+ *
+ * @package    Technical_Test_By_Théo_Soulier_For_Iris_Interactive
+ * @subpackage Technical_Test_By_Théo_Soulier_For_Iris_Interactive/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
---
+++ wp-content/plugins/TEST_IRIS_INTERACTIVE/includes/class-technical test by théo soulier for iris interactive-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://theo-soulier.fr
+ * @since      1.0.0
+ *
+ * @package    Technical_Test_By_Théo_Soulier_For_Iris_Interactive
+ * @subpackage Technical_Test_By_Théo_Soulier_For_Iris_Interactive/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Technical_Test_By_Théo_Soulier_For_Iris_Interactive
+ * @subpackage Technical_Test_By_Théo_Soulier_For_Iris_Interactive/includes
+ * @author     Théo SOULIER <theos.pro@proton.me>
+ */
+class Technical_Test_By_Théo_Soulier_For_Iris_Interactive_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
