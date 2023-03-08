26cee682e55d30fe9f4a6280995236c05dd0a612
ONK <theos.pro@proton.me>
2023-03-08 18:05:17 +0100

    Ok wihtout maps and get API routing

--- wp-content/themes/iris-interactive-theme/functions.php
+++ wp-content/themes/iris-interactive-theme/functions.php
@@ -209,16 +209,12 @@ function do_shortcode_form() {
 	$form_html = '<form method="post">
 		<label for="nom">Nom :</label>
 		<input type="text" name="nom" id="nom" required>
-
 		<label for="prenom">Prénom :</label>
 		<input type="text" name="prenom" id="prenom" required>
-
 		<label for="email">Email :</label>
 		<input type="email" name="email" id="email" required>
-
 		<label for="localisation">Localisation :</label>
 		<input type="text" name="localisation" id="localisation" required>
-
 		<input type="submit" name="submit" value="Envoyer">
 	</form>';
 
